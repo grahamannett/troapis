18dd58f89c6c06f7bc469f21f21e7e21fb8850a6
graham <graham.annett@gmail.com>
2024-08-14 11:09:37 -0600

    Update constants.py

--- src/troapis/constants.py
+++ src/troapis/constants.py
@@ -1,12 +1,10 @@
 from os import environ
 
 DEBUG_MODE = environ.get("DEBUG", "false").lower() in ["true", "1"]
+SAVE_MODE = environ.get("SAVE", "true").lower() in ["true", "1"]
+REQ_SAVE_DIR = environ.get("REQUEST_SAVE_DIR", "out/requests")
 
 ENTRYPOINT_PATH = "model_entrypoint.py"
 MODULE_NAME = "model_entrypoint"
 
-SAVE_MODE = environ.get("DEBUG", "true").lower() in ["true", "1"]
-REQ_SAVE_DIR = environ.get("REQUEST_SAVE_DIR", "out/requests")
-
-# constant but i dont like it uppercase
 device = environ.get("DEVICE", "cuda:0")
